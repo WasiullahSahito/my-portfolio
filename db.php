380cd257b12681cf7443d01c7290cfeabc2c2de5
WasiullahSahito <wasi1237585@gmail.com>
2025-06-04 19:26:24 +0500

    Bug fixed

--- db.php
+++ db.php
@@ -1,5 +1,5 @@
 <?php 
-$con = new mysqli("localhost", "root", "", "my-portfolio");
+$con = new mysqli("localhost", "root", "", "portfolio");
 if($con->connect_error) {
     die("Connection failed: " . $con->connect_error);
 }
